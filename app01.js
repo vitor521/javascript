7a799570d3479635b295dac1aa736c8071c795b0
vitor521 <vitorvfh96@gmail.com>
2025-05-08 20:16:35 -0300

    operadores

--- app01.js
+++ app01.js
@@ -9,19 +9,55 @@ let idade = 28
 console.log(nome)
 console.log(idade)
 nome = "Eloisa"
-idade ="22"
-console.log (nome,idade)
+idade = "22"
+console.log(nome, idade)
+
+// Tipos de dados
+// text == string
+let cidade = "americana"
+
+//numérico == number
+let salario = 1500.35
+
+//booleano == boolean
+let fumante = false
+
+console.log(typeof cidade)
+console.log(typeof salario)
+console.log(typeof fumante)
+
 // Operadores
-//     Operador Atribuição
-//     Operadores aritméticos
-//     Operadores relacionais
-//     Operadores lógicos
+//     Operador Atribuição ( = )
+//     Operadores aritméticos (+, -, /, *, %)
+console.log(10 + 15)
+let n1 = 10
+let n2 = 5
+console.log(n1 + n2) // soma
+console.log(n1 * n2) // multiplicação
+console.log(n1 / n2) // divisão
+console.log(n1 - n2) // subtração
+console.log(n1 % 3) // porcentagem / módulo (resto da divisão)
+
+//     Operadores relacionais (> maior, < menor , >=, <= , != , ==)
+console.log(n1 == n2) // igualdade
+console.log(n1 != n2) // diferente
+console.log(n1 > n2) // maior
+console.log(10 < 10) // menor
+console.log(10 <= 10) // menor igual
+console.log(150 >= 175) // maior igual
+
+//     Operadores lógicos (! não, && e, || ou)
+console.log(!10 > 2) // não - inverte o resultado, se for verdadeiro muda para falso muda pra verdadeiro
+console.log(!false)
+console.log(10 > 2 && 35 < 100 && n1 > n2 && 100 < 90) // E - todas as verificações precisam ser verdadeiras para o resultado ser verdadeiro
+
+console.log(10<2 || 100==150 || 57==57) // OU - apenas uma verificação precisa ser verdadeira para o resultado ser verdadeiro, o resultado só será falso quando TODAS as verificações forem falsas
 
 // Estrutura
 //     Estrutura de controle/decisão
 //     Laços de repetição
 
-// Arrays 
+// Arrays
 
 // Funções básicas
 // Escopo
@@ -43,19 +79,5 @@ console.log (nome,idade)
 
 // Spread e REST  ************
 
-// Tipos de dados
-// text == string
-let cidade = "americana"
 
-//numérico == number
-let salario = 1500.35
 
-//booleano == boolean
-let fumante = false
-
-console.log(typeof cidade)
-console.log(typeof salario)
-console.log(typeof fumante)
-
- 
- 
